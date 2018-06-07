5cc01df26efe8ade9188f420a3a423740a92506a
Pramod More <pramod@biz4solutions.com>
2018-06-07 13:05:40 +0530

    first commit

---
+++ LAbelHorizontalScroll/LAbelHorizontalScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LAbelHorizontalScroll
+//
+//  Created by Dheeraj Gupta on 6/7/18.
+//  Copyright © 2018 Biz4solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
